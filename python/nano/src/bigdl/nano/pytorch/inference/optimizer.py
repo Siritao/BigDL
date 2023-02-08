c2d765d150607c2e0a4e9c4d76b37fda101c5f38
Siritao <siruitao@seu.edu.cn>
2023-02-09 02:29:46 +0800

    update && add channel last && refine qconfig

--- python/nano/src/bigdl/nano/pytorch/inference/optimizer.py
+++ python/nano/src/bigdl/nano/pytorch/inference/optimizer.py
@@ -680,6 +680,7 @@ class InferenceOptimizer(BaseInferenceOptimizer):
             invalidInputError(False,
                               "Now we only support {} device when accelerator"
                               "is openvino.".format(device))
+        model.eval()  # change model to eval mode
         if precision == 'bf16':
             if accelerator is None or accelerator == "jit":
                 if use_ipex or accelerator == "jit":
@@ -1008,6 +1009,7 @@ class InferenceOptimizer(BaseInferenceOptimizer):
             invalidInputError(False,
                               "Now we only support {} device when accelerator "
                               "is openvino.".format(device))
+        model.eval()  # change model to eval mode
         if accelerator == 'openvino':  # openvino backend will not care about ipex usage
             final_openvino_option = {"INFERENCE_PRECISION_HINT": "f32"}
             if openvino_config is not None:
